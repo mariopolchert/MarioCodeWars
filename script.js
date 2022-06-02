716c6f5b7b718676a16360679f594e8d94a2c3dd
mariopolchert <mario.polchert@gmail.com>
2022-06-02 22:04:44 +0200

    just one 7kyu, forgot index in map, tired

--- script.js
+++ script.js
@@ -2347,3 +2347,30 @@
 // console.log(getSum(0, -1)); //-1
 // console.log(getSum(0, 1)); //1
 //---------------------------------------------------------------------------------------------------------------
+// 7 kyu Mumbling
+// This time no story, no theory. The examples below show you how to write function accum:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+// function accum(s) {
+//     let result = [];
+//     for (let i = 0; i < s.length; i++) {
+//         result.push(s[i].repeat(i + 1));
+//     }
+//     return result
+//         .map((w) => w[0].toUpperCase() + w.slice(1).toLowerCase())
+//         .join("-");
+// }
+
+// // TODO function accum(s) {
+// //     return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
+// //   }
+
+// console.log(accum("ZpglnRxqenU")); // "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+// console.log(accum("NyffsGeyylB")); // "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+// console.log(accum("MjtkuBovqrU")); // "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
+// console.log(accum("EvidjUnokmM")); // "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
+// console.log(accum("HbideVbxncC")); // "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"
+//------------------------------------------------------------------------------------------------------------------
