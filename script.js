e7571b451ba8d65d77c271da223af8c44290940e
mariopolchert <mario.polchert@gmail.com>
2022-06-05 17:43:28 +0200

    just one 8kyu, doing wp

--- script.js
+++ script.js
@@ -2741,23 +2741,18 @@
 // console.log(upArray([4, 3, 2, 5])); // [4,3,2,6]
 // console.log(upArray([1, -9])); // null
 //----------------------------------------------------------------------------------------------------
-// 8 kyu Be Concise II - I Need Squares
-// You are given a program squaresOnly that accepts an array of natural numbers up to and including 100 (and including 0) of length >= 1, array, and returns a new array containing only square numbers that have appeared in the input array.
-// Refactor the solution to use as few characters as possible. There is a maximum character limit of 127. Here are a few hints:
-//     There are a lot of handy built-in Array methods in Javascript that you may have never heard of even after completing a basic course in Javascript (e.g. those provided by Codecademy) - well, at least I haven't heard of until quite recently. You may also want to research any new built-in methods offered by ES6, the newest specification of Javascript at the time of writing.
-//     Don't you think the array parameter is a bit wordy? ;)
+// 8 kyu  Be Concise III - Sum Squares
+// sumSquares([1,2,3,4,5]) === 55 // 1 ** 2 + 2 ** 2 + 3 ** 2 + 4 ** 2 + 5 ** 2
+// sumSquares([7,3,9,6,5]) === 200
+// sumSquares([11,13,15,18,2]) === 843
+// Shorten the code such that it meets the requirements.
+// A few hints:
+//     Try researching about built-in Array methods; they may help shorten your code a lot
+// Good luck!
+// const sumSquares = (a) => a.reduce((a, c) => +a + c ** 2);
+// console.log(sumSquares([1, 2, 3, 4, 5])); // 55
+// console.log(sumSquares([7, 3, 9, 6, 5])); // 200
+// console.log(sumSquares([11, 13, 15, 18, 2])); // 843
+// console.log(sumSquares([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5])); // 110
 
-// function squaresOnly(array) {
-//     return array.filter((n) => Number.isInteger(n ** 0.5));
-// }
-
-// console.log(squaresOnly([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // [0,1,4,9]
-// console.log(squaresOnly([31, 33, 35, 37, 39, 38, 36, 34, 32])); // [36]
-// console.log(squaresOnly([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])); // [0,100]
-// console.log(squaresOnly([20, 19, 18, 17, 16, 15, 13, 11, 9, 1, 5, 4])); // [16,9,1,4]
-// console.log(
-//     squaresOnly([
-//         45, 36, 75, 64, 9, 9, 4, 1, 4, 1, 1, 16, 16, 18, 17, 35, 25, 35,
-//     ])
-// ); // [36,64,9,9,4,1,4,1,1,16,16,25]
-//-------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------
