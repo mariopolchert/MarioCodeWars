3dbe5b22236c529eb24b6ea99e4a7d3620e25a1d
mariopolchert <mario.polchert@gmail.com>
2022-06-06 21:12:08 +0200

    just one 8kyu, doing wp

--- script.js
+++ script.js
@@ -2756,3 +2756,26 @@
 // console.log(sumSquares([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5])); // 110
 
 //----------------------------------------------------------------------------------------------------------
+// 8 kyu Lexical this
+// Complete the Person object, by completing the FillFriends function to fill the _friends Array for the person object.
+// Output
+// Return the person object with the Name, Friends and FillFriends function filled out which will fill the input into the Friends property.
+// Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+// var Person = function () {
+//     var person = {
+//         _name: "Leroy",
+//         _friends: [],
+//         fillFriends(f) {
+//             this._friends.push(...f);
+//         },
+//     };
+//     return person;
+// };
+
+// const m = new Person();
+// m.fillFriends("Ana");
+// m.fillFriends("Frank");
+// console.log(m);
+// // expected [ [ 'bob', 'john' ] ] to deeply equal [ 'bob', 'john' ]
+//--------------------------------------------------------------------------------------------------------
