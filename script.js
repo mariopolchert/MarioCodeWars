eaaa899a96f00981c4d076719d6c00fc0ab7c768
mariopolchert <mario.polchert@gmail.com>
2022-06-10 21:34:12 +0200

    one easy 7kyu

--- script.js
+++ script.js
@@ -2796,3 +2796,16 @@
 // console.log(generateLink("colbydauph")); // ("http://www.codewars.com/users/colbydauph");
 // console.log(generateLink("m_polchert")); // 'https://www.codewars.com/users/m_polchert'
 //--------------------------------------------------------------------------------------------------------
+// 7 kyu Sort Numbers
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+// solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+// solution(null); // should return []
+
+// const solution = (n) => (n ? n.sort((a, b) => a - b) : []);
+
+// console.log(solution([1, 2, 3, 10, 5])); // [1,2,3,5,10]
+// console.log(solution(null)); // should return []
+// console.log(solution([])); // []
+// console.log(solution([20, 2, 10])); // [2,10,20]
+// console.log(solution([2, 20, 10])); // [2,10,20]
+//-------------------------------------------------------------------------------------------------
