eee42821b8ccd022671ef83f5c170b722dbf4e95
mariopolchert <mario.polchert@gmail.com>
2022-06-12 15:42:51 +0200

    one 6kyu

--- script.js
+++ script.js
@@ -2876,3 +2876,25 @@
 //         divide: a / b,
 //     }[operator]);
 //--------------------------------------------------------------------------------------
+// 6 kyu  Two Sum
+// Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+// For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+// The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+// Based on: http://oj.leetcode.com/problems/two-sum/
+// twoSum([1, 2, 3], 4) // returns [0, 2] or [2, 0]
+
+// function twoSum(numbers, target) {
+//     for (let i = 0; i < numbers.length; i++) {
+//         for (let j = i + 1; j < numbers.length; j++) {
+//             console.log(numbers[i], numbers[j]);
+//             if (numbers[i] + numbers[j] === target) return [i, j];
+//         }
+//     }
+// }
+
+// TODO twoSum=(n,t,r)=>[r=n.indexOf(n.filter(u=>n.includes(t-u))[0]),n.lastIndexOf(t-n[r])]
+
+// console.log(twoSum([1, 2, 3], 4)); //  sort(numericalCompare), [0,2]);
+// console.log(twoSum([1234, 5678, 9012], 14690)); //  sort(numericalCompare), [1,2]);
+// console.log(twoSum([2, 2, 3], 4)); //  sort(numericalCompare), [0,1]);
+//--------------------------------------------------------------------------------------------
