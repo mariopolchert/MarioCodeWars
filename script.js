4d47d368130495fe9af9227e1954f68b27cc4a6c
mariopolchert <mario.polchert@gmail.com>
2022-07-02 22:14:57 +0200

    just one 8kyu

--- script.js
+++ script.js
@@ -3068,3 +3068,19 @@
 // console.log(toWeirdCase("is")); // 'Is'
 // console.log(toWeirdCase("This is a test")); // 'ThIs Is A TeSt'
 //-------------------------------------------------------------------------------------------
+// 8 kyu Invert values
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+// You can assume that all values are integers. Do not mutate the input array/list.
+
+// const invert = (a) => a.map((n) => n * -1);
+
+// TODO const invert = array => array.map(num => -num);
+
+// console.log(invert([1, 2, 3, 4, 5])); // [-1,-2,-3,-4,-5]
+// console.log(invert([1, -2, 3, -4, 5])); // [-1,2,-3,4,-5]
+// console.log(invert([])); // []
+// console.log(invert([0])); // [-0]
+//---------------------------------------------------------------------------------------
