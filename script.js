9fd2d485c10341310ec04afd27d35cf943605ef9
Mario Polchert <mario.polchert@gmail.com>
2023-02-23 21:55:46 +0100

    one kyu6

--- script.js
+++ script.js
@@ -3331,3 +3331,33 @@
 // console.log(squareSum([0, 3, 4, 5])); //50
 // console.log(squareSum([])); // 0
 //-------------------------------------------------------------------------------------
+// 6 kyu Multiplication table
+// Your task, is to create N×N multiplication table, of size provided in parameter.
+// For example, when given size is 3:
+// 1 2 3
+// 2 4 6
+// 3 6 9
+// For the given example, the return value should be:
+// [[1,2,3],[2,4,6],[3,6,9]]
+
+// 1 2 3 4
+// 2 4 6 8
+// 3 6 9 12
+// 4 8 12 16
+
+// const multiplicationTable = function (size) {
+//     const result = [];
+//     for (let j = 1; j <= size; j++) {
+//         let temp = [];
+//         for (let i = 1; i <= size; i++) {
+//             temp.push(j * i);
+//         }
+//         result.push(temp);
+//     }
+//     return result;
+// };
+
+// console.log(multiplicationTable(5));
+// console.log(multiplicationTable(4));
+// console.log(multiplicationTable(3));
+//--------------------------------------------------------------------------
