8ef75a924d3457daa37ee270c81da141f957b419
Mario Polchert <mario.polchert@gmail.com>
2023-02-27 15:46:31 +0100

    one 6kyu

--- script.js
+++ script.js
@@ -3489,3 +3489,86 @@
 // console.log(revrot("123456987653", 6)); // "234561356789"
 // console.log(revrot("66443875", 4)); // "44668753"
 //-----------------------------------------------------------------------------------
+// 6 kyu The Vowel Code
+// Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern:
+// a -> 1
+// e -> 2
+// i -> 3
+// o -> 4
+// u -> 5
+// For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels in this kata.
+// Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
+// For example, decode("h3 th2r2") would return "hi there".
+// For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
+// Arrays
+// Strings
+// Regular Expressions
+// Fundamentals
+
+// function encode(string) {
+//     let result = "";
+//     for (let l of string) {
+//         switch (l) {
+//             case "a":
+//                 result += "1";
+//                 break;
+//             case "e":
+//                 result += "2";
+//                 break;
+//             case "i":
+//                 result += "3";
+//                 break;
+//             case "o":
+//                 result += "4";
+//                 break;
+//             case "u":
+//                 result += "5";
+//                 break;
+//             default:
+//                 result += l;
+//         }
+//     }
+//     return result;
+// }
+
+// function decode(string) {
+//     let result = "";
+//     for (let l of string) {
+//         switch (l) {
+//             case "1":
+//                 result += "a";
+//                 break;
+//             case "2":
+//                 result += "e";
+//                 break;
+//             case "3":
+//                 result += "i";
+//                 break;
+//             case "4":
+//                 result += "o";
+//                 break;
+//             case "5":
+//                 result += "u";
+//                 break;
+//             default:
+//                 result += l;
+//         }
+//     }
+//     return result;
+// }
+
+// console.log(encode("hello")); // 'h2ll4'
+// console.log(encode("How are you today?")); // 'H4w 1r2 y45 t4d1y?'
+// console.log(encode("This is an encoding test.")); // 'Th3s 3s 1n 2nc4d3ng t2st.'
+// console.log(decode("h2ll4")); // 'hello'
+
+// // turn vowels into numbers
+// function encode(string){
+// return string.replace(/[aeiou]/g, function (x) { return '_aeiou'.indexOf(x) });
+// }
+
+// //turn numbers back into vowels
+// function decode(string){
+// return string.replace(/[1-5]/g, function (x) { return '_aeiou'.charAt(x) });
+// }
+//------------------------------------------------------------------------------------
